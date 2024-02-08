11dbc83816d5796b2c3875d91a83a81065e86dd6
Nelson Mabini <nymabini@apc.edu.ph>
2024-02-08 18:22:53 +0800

    forms updated

--- week8/forms.php
+++ week8/forms.php
@@ -126,5 +126,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+
+// For apcwebprog
+$servername = "localhost";
+$username = "webprogss221";
+$password = "=latHen97";
+$dbname = "webprogss221";
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO nymabini_myguests (name, email,website,comment,gender)
+VALUES ('$name', '$email','$website','$comment','$gender')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+}
+?>
+
+
+
+
 </body>
 </html>

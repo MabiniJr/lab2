2ad273924f6c1ea2c0b238f61c11c92a048bead0
Nelson Mabini Jr <122596061+MabiniJr@users.noreply.github.com>
2024-02-09 17:54:31 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM jlquesada_myguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM nymabini_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
